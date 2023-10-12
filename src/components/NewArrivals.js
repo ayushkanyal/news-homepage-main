992c25cd5b2a47cc261ab8f723e8122e2a39b558
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-12 22:52:28 +0530

    Update NewArrivals.js
    Added new classes to support CSS styling finish

--- src/components/NewArrivals.js
+++ src/components/NewArrivals.js
@@ -1,14 +1,15 @@
-import styles from "./NewArrivals.css";
+import styles from "./NewArrivals.module.css";
 
 export default function NewArrivals() {
   return (
     <>
-      <section className={styles.new}>
-        <h2>New</h2>
+      <section className={styles["new"]}>
+        <h1>New</h1>
         <article>
           <h3>Hydrogen VS Electric Cars</h3>
           <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
         </article>
+        <div className={styles["break"]}></div>
         <article>
           <h3>The Downsides of AI Artistry</h3>
           <p>
@@ -16,6 +17,7 @@ export default function NewArrivals() {
             generation?
           </p>
         </article>
+        <div className={styles["break"]}></div>
         <article>
           <h3>Is VC Funding Drying Up?</h3>
           <p>
