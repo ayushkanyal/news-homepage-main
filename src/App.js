22cc2b1c8b1dd6c1786494b65b606b4643291f74
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-12 22:55:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,32 @@
-import { useState } from "react";
-import List from "./components/Output/List";
-import Form from "./components/Input/Form";
-import ErrorModal from "./components/Output/ErrorModal";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import TitleCover from "./components/TitleCover";
+import NewArrivals from "./components/NewArrivals";
+import PopulaArticles from "./components/PopularArticles";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [error, setError] = useState(false);
-  const [inputs, setInputs] = useState([]);
-
-  function handleClick() {
-    setError(false);
-  }
-
-  function fetchSubmit(input) {
-    console.log(input.username.length);
-    console.log(input.age.length);
-    if (input.username.length === 0) {
-      setError({
-        title: "Invalid Username",
-        message: "Please Enter a valid Name.",
-      });
-      return;
-    }
-    if (input.age <= 0) {
-      setError({
-        title: "Invalid Age",
-        message: "Please Enter a valid Age(>0).",
-      });
-      return;
-    } else {
-      setName((values) => [...values, input.username]);
-      setAge((values) => [...values, input.age]);
-      setInputs((values) => [...values, input]);
-    }
-  }
-
   return (
-    <div>
-      <Form onAdd={fetchSubmit} />
-      <List list={inputs} />
-      {error && (
-        <ErrorModal
-          title={error.title}
-          text={error.message}
-          onClick={handleClick}
-        />
-      )}
+    <div className="App">
+      <main>
+        <Navbar />
+        <TitleCover />
+        <NewArrivals />
+        <PopulaArticles />
+        <footer>
+          <div className="attribution">
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Frontend Mentor
+            </a>
+            . Coded by <a href="https://github.com/ayushkanyal">Ayush Kanyal</a>
+            .
+          </div>
+        </footer>
+      </main>
     </div>
   );
 }
