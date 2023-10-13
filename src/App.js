67b0d741345cd2906800a106ed04878e813a7a20
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-13 23:49:17 +0530

    Update App.js
    Added relevant classes for responsive website

--- src/App.js
+++ src/App.js
@@ -7,26 +7,31 @@ import PopulaArticles from "./components/PopularArticles";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <main>
-        <Navbar />
-        <TitleCover />
-        <NewArrivals />
-        <PopulaArticles />
-        <footer>
-          <div className="attribution">
-            Challenge by{" "}
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Frontend Mentor
-            </a>
-            . Coded by <a href="https://github.com/ayushkanyal">Ayush Kanyal</a>
-            .
-          </div>
-        </footer>
+        <div className="block_01">
+          <TitleCover />
+        </div>
+        <div className="block_02">
+          <NewArrivals />
+        </div>
+        <div className="block_03">
+          <PopulaArticles />
+        </div>
       </main>
+      <footer>
+        <div className="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/ayushkanyal">Ayush Kanyal</a>.
+        </div>
+      </footer>
     </div>
   );
 }
