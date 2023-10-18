50fb8bec3b7296db59a401c5fb5ccfac75182875
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-19 02:07:52 +0530

    Update TitleCover.js
    Added the paragraph and button element inside the same "grid_container" class div to avoid pushing away the button

--- src/components/TitleCover.js
+++ src/components/TitleCover.js
@@ -12,12 +12,14 @@ export default function TitleCover() {
           <img src={desktopImage} alt="Grid puzzle desktop"></img>
         </picture>
         <h1>The Bright Future of Web 3.0?</h1>
-        <p>
-          We dive into the next evolution of the web that claims to put the
-          power of the platforms back into the hands of the people. But is it
-          really fulfilling its promise?{" "}
-        </p>
-        <button>Read more</button>
+        <div className={style["grid_container"]}>
+          <p>
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?{" "}
+          </p>
+          <button>Read more</button>
+        </div>
       </section>
     </>
   );
